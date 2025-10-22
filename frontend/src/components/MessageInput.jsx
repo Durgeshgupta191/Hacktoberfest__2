1ab2d02cc11592c6a5dbb31188b60d1f2b95a33c
Pritam Kumar <pritamk6284987295@gmail.com>
2025-10-22 12:17:32 +0530

    fix

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -303,8 +303,6 @@ const MessageInput = ({ isGroup = false }) => {
             type="button"
             className={`hidden sm:flex btn btn-circle
                      ${imagePreview ? "text-emerald-500" : "text-zinc-400"}`}
-            className={`flex btn btn-circle btn-sm sm:btn-md
-                     ${imagePreview ? 'text-emerald-500' : 'text-zinc-400'}`}
             onClick={() => fileInputRef.current?.click()}
           >
             <Image size={20} />
